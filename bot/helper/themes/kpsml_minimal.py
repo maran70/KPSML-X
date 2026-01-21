92ab8da0504e8c3c75bb8a9d846037239247f85e
maran70 <141546842+maran70@users.noreply.github.com>
2026-01-21 19:28:18 +0530

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -3,9 +3,9 @@ class KPSMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = '🤖 Repo'
-    ST_BN1_URL = 'https://github.com/Tamilupdates/KPSML-X'
+    ST_BN1_URL = 'https://github.com/maran70/KPSML-X'
     ST_BN2_NAME = 'Updates 🔥'
-    ST_BN2_URL = 'https://telegram.me/KPSBots'
+    ST_BN2_URL = 'https://t.me/AnimeS_LinkzZ'
     ST_MSG = '''<b><i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n
 Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
@@ -39,7 +39,7 @@ Type {help_command} to get a list of available commands</b>'''
     CLOSE_BT = 'Close'
     HELP_HEADER = "㊂ <b><i>Help Guide Menu!</i></b>\n\n<b>NOTE: <i>Click on any CMD to see more minor detalis.</i></b>"
 
-    # async def stats(client, message):
+   # async def stats(client, message):
     BOT_STATS = '''⌬ <b><i>BOT STATISTICS :</i></b>
 ┖ <b>Bot Uptime :</b> {bot_uptime}
 
@@ -108,12 +108,12 @@ Type {help_command} to get a list of available commands</b>'''
     # ---------------------
 
     # async def restart_notification(): ---> __main__.py
-    RESTART_SUCCESS = '''⌬ <b><i>Restarted Successfully!</i></b>
-┠ <b>Date:</b> {date}
-┠ <b>Time:</b> {time}
-┠ <b>TimeZone:</b> {timz}
-┖ <b>Version:</b> {version}'''
-    RESTARTED = '''⌬ <b><i>Bot Restarted!</i></b>'''
+    RESTART_SUCCESS = '''⌬ 🟢 <b><i>Restarted Successfully!</i></b>
+┠ 🗓️ <b>Date:</b> {date}
+┠ ⌚ <b>Time:</b> {time}
+┠ 🌍 <b>TimeZone:</b> {timz}
+┖ ⚙️ <b>Version:</b> {version}'''
+    RESTARTED = '''⌬ ⚡ <b><i>Bot Restarted!</i></b>'''
     # ---------------------
 
     # async def ping(client, message): ---> __main__.py
@@ -131,15 +131,15 @@ Type {help_command} to get a list of available commands</b>'''
 {Source}
 ------------------------------------------\n\n"""
     
-    # async def __msg_to_reply(self): ---> pyrogramEngine.py
-    PM_START =            "➲ <b><u>Task Started :</u></b>\n┃\n┖ <b>Link:</b> <a href='{msg_link}'>Click Here</a>"
-    L_LOG_START =           "➲ <b><u>Leech Started :</u></b>\n┃\n┠ <b>User :</b> {mention} ( #ID{uid} )\n┖ <b>Source :</b> <a href='{msg_link}'>Click Here</a>"
+   # async def __msg_to_reply(self): ---> pyrogramEngine.py
+    PM_START =            "➲ 🚧 <b><u>Task Started :</u></b>\n┃\n┖ 👉 <b>Link:</b> <a href='{msg_link}'>Click Here</a>"
+    L_LOG_START =           "➲ 🧲 <b><u>Leech Started :</u></b>\n┃\n┠ 🤖 <b>User :</b> {mention} ( #ID{uid} )\n┖ 🌐 <b>Source :</b> <a href='{msg_link}'>Click Here</a>"
 
-    # async def onUploadComplete(): ---> tasks_listener.py
-    NAME =                  '<b><i>{Name}</i></b>\n┃\n'
-    SIZE =                  '┠ <b>Size: </b>{Size}\n'
-    ELAPSE =                '┠ <b>Elapsed: </b>{Time}\n'
-    MODE =                  '┠ <b>Mode: </b>{Mode}\n'
+   # async def onUploadComplete(): ---> tasks_listener.py
+    NAME =                  '📁 <b><i>{Name}</i></b>\n┃\n'
+    SIZE =                  '┠ 📦 <b>Size: </b>{Size}\n'
+    ELAPSE =                '┠ ⏳ <b>Elapsed: </b>{Time}\n'
+    MODE =                  '┠ 💠 <b>Mode: </b>{Mode}\n'
 
     # ----- LEECH -------
     L_TOTAL_FILES =         '┠ <b>Total Files: </b>{Files}\n'
@@ -173,48 +173,48 @@ Type {help_command} to get a list of available commands</b>'''
 
     # def get_readable_message(): ---> bot_utilis.py
     ####--------OVERALL MSG HEADER----------
-    STATUS_NAME =       '<b><i>{Name}</i></b>'
+    STATUS_NAME =       '📁 <b><i>{Name}</i></b>'
 
     #####---------PROGRESSIVE STATUS-------
-    BAR =               '\n┃ {Bar}'
-    PROCESSED =         '\n┠ <b>Processed:</b> {Processed}'
-    STATUS =            '\n┠ <b>Status:</b> <a href="{Url}">{Status}</a>'
-    ETA =                                                ' | <b>ETA:</b> {Eta}'
-    SPEED =             '\n┠ <b>Speed:</b> {Speed}'
-    ELAPSED =                                     ' | <b>Elapsed:</b> {Elapsed}'
-    ENGINE =            '\n┠ <b>Engine:</b> {Engine}'
-    STA_MODE =          '\n┠ <b>Mode:</b> {Mode}'
-    SEEDERS =           '\n┠ <b>Seeders:</b> {Seeders} | '
-    LEECHERS =                                           '<b>Leechers:</b> {Leechers}'
+    BAR =               '\n🧲⚡ <b>Charging</b>\n{Bar}\n'
+    PROCESSED =         '\n┠ 📥 <b>Processed:</b> {Processed}'
+    STATUS =            '\n┠ 📜 <b>Status:</b> <a href="{Url}">{Status}</a>'
+    ETA =                                                ' | ⏳ <b>ETA:</b> {Eta}'
+    SPEED =             '\n┠ 🚀 <b>Speed:</b> {Speed}'
+    ELAPSED =                                     ' | ⏱️ <b>Elapsed:</b> {Elapsed}'
+    ENGINE =            '\n┠ ⚙️ <b>Engine:</b> {Engine}'
+    STA_MODE =          '\n┠ 💠 <b>Mode:</b> {Mode}'
+    SEEDERS =           '\n┠ 🟢 <b>Seeders:</b> {Seeders} | '
+    LEECHERS =                                           '🔴 <b>Leechers:</b> {Leechers}'
 
-    ####--------SEEDING----------
-    SEED_SIZE =      '\n┠ <b>Size: </b>{Size}'
-    SEED_SPEED =     '\n┠ <b>Speed: </b> {Speed} | '
-    UPLOADED =                                     '<b>Uploaded: </b> {Upload}'
-    RATIO =          '\n┠ <b>Ratio: </b> {Ratio} | '
-    TIME =                                         '<b>Time: </b> {Time}'
-    SEED_ENGINE =    '\n┠ <b>Engine:</b> {Engine}'
+  ####--------SEEDING----------
+    SEED_SIZE =      '\n┠ 💾 <b>Size: </b>{Size}'
+    SEED_SPEED =     '\n┠ 🚀 <b>Speed: </b> {Speed} | '
+    UPLOADED =                                     '📤 <b>Uploaded: </b> {Upload}'
+    RATIO =          '\n┠ 📊 <b>Ratio: </b> {Ratio} | '
+    TIME =                                         '⏳ <b>Time: </b> {Time}'
+    SEED_ENGINE =    '\n┠ ⚙️ <b>Engine:</b> {Engine}'
 
     ####--------NON-PROGRESSIVE + NON SEEDING----------
-    STATUS_SIZE =    '\n┠ <b>Size: </b>{Size}'
-    NON_ENGINE =     '\n┠ <b>Engine:</b> {Engine}'
+    STATUS_SIZE =    '\n┠ 💾 <b>Size: </b>{Size}'
+    NON_ENGINE =     '\n┠ ⚙️ <b>Engine:</b> {Engine}'
 
-    ####--------OVERALL MSG FOOTER----------
-    USER =              '\n┠ <b>User:</b> <code>{User}</code> | '
-    ID =                                                        '<b>ID:</b> <code>{Id}</code>'
-    BTSEL =          '\n┠ <b>Select:</b> {Btsel}'
-    CANCEL =         '\n┖ {Cancel}\n\n'
+   ####--------OVERALL MSG FOOTER----------
+    USER =              '\n┠ 🧑 <b>User:</b> <code>{User}</code> | '
+    ID =                                                        '🏷️ <b>ID:</b> <code>{Id}</code>'
+    BTSEL =          '\n┠ 🔘 <b>Select:</b> {Btsel}'
+    CANCEL =         '\n┖ 🚫 {Cancel}\n\n'
 
     ####------FOOTER--------
-    FOOTER = '⌬ <b><i>Bot Stats</i></b>\n'
-    TASKS =  '┠ <b>Tasks:</b> {Tasks}\n'
-    BOT_TASKS = '┠ <b>Tasks:</b> {Tasks}/{Ttask} | <b>AVL:</b> {Free}\n'
-    Cpu = '┠ <b>CPU:</b> {cpu}% | '
-    FREE =                      '<b>F:</b> {free} [{free_p}%]'
-    Ram = '\n┠ <b>RAM:</b> {ram}% | '
-    uptime =                     '<b>UPTIME:</b> {uptime}'
-    DL = '\n┖ <b>DL:</b> {DL}/s | '
-    UL =                        '<b>UL:</b> {UL}/s'
+    FOOTER = '⌬ <b><i>📊 Bot Stats</i></b>\n'
+    TASKS =  '📌 <b>Tasks:</b> {Tasks}\n'
+    BOT_TASKS = '📌 <b>Tasks:</b> {Tasks}/{Ttask} | <b>AVL:</b> {Free}\n'
+    Cpu = '💻 <b>CPU:</b> {cpu}% | '
+    FREE =                      '<b>📀 FREE:</b> {free} [{free_p}%]'
+    Ram = '\n🧠 <b>RAM:</b> {ram}% | '
+    uptime =                     '<b>⏱️ UPTIME:</b> {uptime}'
+    DL = '\n📥 <b>DL:</b> {DL}/s | '
+    UL =                        '<b>📤 UL:</b> {UL}/s'
 
     ###--------BUTTONS-------
     PREVIOUS = '⫷'
@@ -244,9 +244,9 @@ Type {help_command} to get a list of available commands</b>'''
 
     # async def mirror_status(_, message): ----> status.py
     NO_ACTIVE_DL = '''<i>No Active Downloads!</i>
-    
+
 ⌬ <b><i>Bot Stats</i></b>
-┠ <b>CPU:</b> {cpu}% | <b>F:</b> {free} [{free_p}%]
+┠ <b>CPU:</b> {cpu}% | <b>FREE:</b> {free} [{free_p}%]
 ┖ <b>RAM:</b> {ram} | <b>UPTIME:</b> {uptime}
     '''
     # ---------------------
