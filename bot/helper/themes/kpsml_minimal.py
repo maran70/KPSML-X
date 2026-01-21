ef53e56d0d9261b4b62788be1b43325840fbac85
maran70 <141546842+maran70@users.noreply.github.com>
2026-01-21 21:35:45 +0530

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -206,7 +206,7 @@ Type {help_command} to get a list of available commands</b>'''
     CANCEL =         '\n┖ 🚫 {Cancel}\n\n'
 
     ####------FOOTER--------
-    FOOTER = '⌬ <b><i>📊 Bot Stats</i></b>\n'
+    FOOTER = '<b><i>📊 Bot Stats</i></b>\n'
     TASKS =  '📌 <b>Tasks:</b> {Tasks}\n'
     BOT_TASKS = '📌 <b>Tasks:</b> {Tasks}/{Ttask} | <b>AVL:</b> {Free}\n'
     Cpu = '💻 <b>CPU:</b> {cpu}% | '
