b16edc120f758692fb44679990cabbd9f2fbcdc7
maran70 <141546842+maran70@users.noreply.github.com>
2026-01-22 07:59:21 +0530

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -136,7 +136,7 @@ Type {help_command} to get a list of available commands</b>'''
     L_LOG_START =           "➲ 🧲 <b><u>Leech Started :</u></b>\n┃\n┠ 🤖 <b>User :</b> {mention} ( #ID{uid} )\n┖ 🌐 <b>Source :</b> <a href='{msg_link}'>Click Here</a>"
 
    # async def onUploadComplete(): ---> tasks_listener.py
-    NAME =                  '📁 <b><i>{Name}</i></b>\n┃\n'
+    NAME =                  '📁 <b><i>{Name}</i></b>\n\n'
     SIZE =                  '📦 <b>Size: </b>{Size}\n'
     ELAPSE =                '⏳ <b>Elapsed: </b>{Time}\n'
     MODE =                  '💠 <b>Mode: </b>{Mode}\n'
